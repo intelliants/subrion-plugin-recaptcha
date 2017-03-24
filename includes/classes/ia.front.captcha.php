19ed8500ae0109dd15cde91c62c8de7c08558f35
Janur Jangaraev <jjangaraev@intelliants.com>
2017-03-24 10:36:44 +0600

    Captcha should pass already authorized user

--- includes/classes/ia.front.captcha.php
+++ includes/classes/ia.front.captcha.php
@@ -1,4 +1,5 @@
 <?php
+
 /******************************************************************************
  *
  * Subrion - open source content management system
@@ -26,69 +27,68 @@
 
 class iaCaptcha extends abstractCore
 {
-	// Get a key from https://www.google.com/recaptcha
-	protected $_publicKey = '';
-	protected $_privateKey = '';
+    // Get a key from https://www.google.com/recaptcha
+    protected $_publicKey = '';
+    protected $_privateKey = '';
 
-	protected $_theme;
+    protected $_theme;
 
-	protected $_error;
+    protected $_error;
 
 
-	public function __construct()
-	{
-		parent::init();
+    public function __construct()
+    {
+        parent::init();
 
-		require_once dirname(__FILE__) . IA_DS . '../src/autoload.php';
+        require_once dirname(__FILE__) . IA_DS . '../src/autoload.php';
 
-		$this->_publicKey = $this->iaCore->get('recaptcha_publickey');
-		$this->_privateKey = $this->iaCore->get('recaptcha_privatekey');
-		$this->_theme = $this->iaCore->get('recaptcha_theme');
+        $this->_publicKey = $this->iaCore->get('recaptcha_publickey');
+        $this->_privateKey = $this->iaCore->get('recaptcha_privatekey');
+        $this->_theme = $this->iaCore->get('recaptcha_theme');
 
-		if ($this->_privateKey)
-		{
-			$this->reCaptcha = new \ReCaptcha\ReCaptcha($this->_privateKey, new \ReCaptcha\RequestMethod\CurlPost());
-		}
-	}
+        if ($this->_privateKey) {
+            $this->reCaptcha = new \ReCaptcha\ReCaptcha($this->_privateKey, new \ReCaptcha\RequestMethod\CurlPost());
+        }
+    }
 
-	public function getImage()
-	{
-		if (!$this->_publicKey || !$this->_privateKey)
-		{
-			return iaLanguage::get('recaptcha_set_configuration');
-		}
+    public function getImage()
+    {
+        if (!$this->_publicKey || !$this->_privateKey) {
+            return iaLanguage::get('recaptcha_set_configuration');
+        }
 
-		$theme = '';
-		if ('light' != $this->iaCore->get('recaptcha_theme'))
-		{
-			$theme = ' data-theme="' . $this->iaCore->get('recaptcha_theme') . '"';
-		}
+        $theme = '';
+        if ('light' != $this->iaCore->get('recaptcha_theme')) {
+            $theme = ' data-theme="' . $this->iaCore->get('recaptcha_theme') . '"';
+        }
 
-		$code = <<<CODE
+        $code = <<<CODE
 <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 <div class="g-recaptcha" data-sitekey="{$this->_publicKey}"{$theme}></div>
 CODE;
 
-		return $code;
-	}
+        return $code;
+    }
 
-	public function validate()
-	{
-		if (!empty($_POST["g-recaptcha-response"]))
-		{
-			$response = $this->reCaptcha->verify($_POST["g-recaptcha-response"], $_SERVER["REMOTE_ADDR"]);
+    public function validate()
+    {
+        if (iaUsers::hasIdentity()) {
+            return true;
+        }
 
-			if ($response != null && $response->isSuccess())
-			{
-				return true;
-			}
-		}
+        if (!empty($_POST["g-recaptcha-response"])) {
+            $response = $this->reCaptcha->verify($_POST["g-recaptcha-response"], $_SERVER["REMOTE_ADDR"]);
 
-		return false;
-	}
+            if ($response != null && $response->isSuccess()) {
+                return true;
+            }
+        }
 
-	public function getPreview()
-	{
-		return $this->getImage();
-	}
+        return false;
+    }
+
+    public function getPreview()
+    {
+        return $this->getImage();
+    }
 }
